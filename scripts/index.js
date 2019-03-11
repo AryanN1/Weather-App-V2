2ddc0e3d6af07f064eb0c4d076dc046a6858783d
Aryan Naimi <aryannaimi@gmail.com>
2019-03-11 12:55:51 -0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,3 @@
-// Modal Parts//
-var modal = document.getElementById('myModal');
-
-var btn = document.getElementById("myBtn");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 // App Start//
 const UI = (function () {
   let menu = document.querySelector("#menu-container");
